26dc06e7313658f20f6a4a0e291b01fc791d6bf2
rose <ora0002@auburn.edu>
2019-12-16 16:06:20 -0600

    commit

--- NanoWeb/src/Main.java
+++
@@ -1,9 +0,0 @@
-import java.io.IOException;
-import java.net.ServerSocket;
-
-class NanoWebServer {
-
-    public static void main(String[] args) throws IOException {
-        ServerSocket server = new ServerSocket(8888);
-    }
-}
